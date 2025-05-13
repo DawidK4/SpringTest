5a026b683b15e687597da6674f7ffcaaf18a6839
DawidK4 <s27637@pjwstk.edu.pl>
2025-05-13 15:01:18 +0200

    init

---
+++ s27637_MP5/src/main/java/edu/pja/mas/dkucharski/s27637_mp5/S27637Mp5Application.java
@@ -0,0 +1,13 @@
+package edu.pja.mas.dkucharski.s27637_mp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27637Mp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S27637Mp5Application.class, args);
+    }
+
+}
---
+++ s27637_MP5/src/test/java/edu/pja/mas/dkucharski/s27637_mp5/S27637Mp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pja.mas.dkucharski.s27637_mp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S27637Mp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
